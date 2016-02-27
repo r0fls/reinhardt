df2d1d7c7db0204adc949aef43b94fd30720fe02
r0fls <raphael.deem@gmail.com>
2016-02-27 12:58:00 -0800

    regexp handler

--- src/url/url.go
+++ src/url/url.go
@@ -3,10 +3,49 @@ package url
 import (
 	//"github.com/r0fls/reinhardt/src/view"
 	"net/http"
+	"regexp"
 )
 
 type Url struct {
 	Slug string
 	View func(http.ResponseWriter, *http.Request)
 	//View view.View
+}
+
+type route struct {
+	pattern *regexp.Regexp
+	handler http.Handler
+}
+
+type RegexpHandler struct {
+	routes []*route
+}
+
+func New(urls []Url) RegexpHandler {
+	r := make([]*route, len(urls))
+	return RegexpHandler{r}
+}
+
+func (h *RegexpHandler) handler(pattern *regexp.Regexp, handler http.Handler) {
+	h.routes = append(h.routes, &route{pattern, handler})
+}
+
+func (h *RegexpHandler) Handler(pattern string, handler http.Handler) {
+	re, _ := regexp.Compile(pattern)
+	h.routes = append(h.routes, &route{re, handler})
+}
+
+func (h *RegexpHandler) HandleFunc(pattern *regexp.Regexp, handler func(http.ResponseWriter, *http.Request)) {
+	h.routes = append(h.routes, &route{pattern, http.HandlerFunc(handler)})
+}
+
+func (h *RegexpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for _, route := range h.routes {
+		if route.pattern.MatchString(r.URL.Path) {
+			route.handler.ServeHTTP(w, r)
+			return
+		}
+	}
+	// no pattern matched; send 404 response
+	http.NotFound(w, r)
 }
--- src/view/view.go
+++ src/view/view.go
@@ -2,7 +2,6 @@ package view
 
 import (
 	"fmt"
-	//	"github.com/julienschmidt/httprouter"
 	"github.com/r0fls/reinhardt/src/config"
 	"html"
 	"io/ioutil"
@@ -20,25 +19,10 @@ func Render(template string, r Request) {
 	dir, _ := os.Getwd()
 	f := strings.Join([]string{dir, "settings.json"}, "/")
 	config := config.Load_config(f)
-	//text := respool.Read(strings.Join(append(base, template), "/"))
 	f = strings.Join([]string{config.Home, config.Apps[0], config.Templates[0], template}, "/")
 	text, err := ioutil.ReadFile(f)
 	check(err)
 	fmt.Fprintf(r.Response, string(text), html.EscapeString(r.Request.URL.Path))
-	/*
-		var text []byte
-		var err error
-
-		for i, _ := range config.Templates {
-			print(strings.Join([]string{config.Home, config.Templates[i], template}, "/"), "\n")
-			text, err = ioutil.ReadFile(strings.Join([]string{config.Home, config.Templates[i], template}, "/"))
-			if err == nil {
-				fmt.Fprintf(r.Response, string(text), html.EscapeString(r.Request.URL.Path))
-				break
-			}
-			print(string(text))
-		}
-	*/
 }
 
 type View func(template string, base []string) func(w http.ResponseWriter, r *http.Request)
